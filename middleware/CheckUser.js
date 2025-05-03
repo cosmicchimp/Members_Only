42808a73f3999d51249029943cbecde5a2d4cb29
cosmicchimp <max.lambert429@gmail.com>
2025-05-03 00:10:18 -0600

    Adding onto the sign in function

--- middleware/CheckUser.js
+++ middleware/CheckUser.js
@@ -8,7 +8,18 @@ router.use(express.json());
 router.post("/login", async (req, res) => {
     try { const {username, password} = req.body
     const userData = await sql`SELECT * FROM users WHERE username = ${username}`
-    console.log(userData)}
+    console.log(userData)
+if (userData.length < 1) {
+    return res.send({message:"Account not found"})
+}
+const passCheck = await bcrypt.compare(password, userData[0].password)
+if (passCheck) {
+    res.send({success: true})
+}
+else {
+    res.send({success: false})
+}}
+
     catch (e) {console.log("Error in checkuser: ", e)}
    
 })
