5c6189d98bcb2b3c332e611a41f6ace480cb80f5
cosmicchimp <max.lambert429@gmail.com>
2025-05-02 23:02:02 -0600

    Creating sign up function

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const HomeRoute = require("./routes/HomeRoute")
 const SignupRoute = require("./routes/SignUpRoute")
+const StoreUser = require("./middleware/StoreUser")
 const path = require("path");  
 const app = express()
 app.use(express.static(path.join(__dirname, 'public'))); 
 app.set('view engine', 'ejs');
-app.use('/', HomeRoute)
-app.use("/signup", SignupRoute)
+app.use(HomeRoute)
+app.use(SignupRoute)
+app.use(StoreUser)
 app.listen(4000, () => {
     console.log("Server running on local")
 })
