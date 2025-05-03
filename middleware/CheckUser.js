c305da09007f2732eca677cc94fb0d86d03fe91d
cosmicchimp <max.lambert429@gmail.com>
2025-05-03 00:02:49 -0600

    Creating sign in function

--- middleware/CheckUser.js
+++ middleware/CheckUser.js
@@ -6,7 +6,7 @@ const router = express.Router()
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 router.post("/login", async (req, res) => {
-    try { const username = res.body.username
+    try { const {username, password} = req.body
     const userData = await sql`SELECT * FROM users WHERE username IN ${username}`
     console.log(userData)}
     catch (e) {console.log("Error in checkuser: ", e)}
