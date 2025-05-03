be1079322c5468374c87a3700a21553a556723de
cosmicchimp <max.lambert429@gmail.com>
2025-05-02 23:35:19 -0600

    dotenv add

--- routes/SignUpRoute.js
+++ routes/SignUpRoute.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-router.get("/", (req, res) => {
+router.get("/signup", (req, res) => {
     res.render("SignupView")
 })
 module.exports = router
