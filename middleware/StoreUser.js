7aea9a8451d2c16c3b3866866d3f7e7c628dc367
cosmicchimp <max.lambert429@gmail.com>
2025-05-02 22:48:17 -0600

    Creating sign up function

--- middleware/StoreUser.js
+++ middleware/StoreUser.js
@@ -5,11 +5,14 @@ const neon = require("@neondatabase/serverless");
 const dotenv = require("dotenv").config()
 const sql = neon(process.env.DATABASE_URL)
 const router = express.Router()
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
 router.post("https://devsocial.up.railway.app/signup", async (req, res) => {
     try {
         const {username, password} = req.body
         const cryptedPass = bcrypt.hash(password, 10)
         await sql`INSERT INTO mo.users(username, password) VALUES (${username}, ${cryptedPass})`
+        res.send("project created")
     }
     catch (e) {
         console.log("Error in user sign up server side... StoreUser.js: ", e)
